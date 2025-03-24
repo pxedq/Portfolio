b70f10d0d7780c68415ccfc145c8047cc65263a1
pxedq <nagynandi2007@gmail.com>
2025-03-24 13:39:33 +0100

    1

---
+++ main.js
@@ -0,0 +1,45 @@
+const url = "https://raw.githubusercontent.com/pxedq/PortfolioJSON/refs/heads/main/portfolio.json"
+
+function CreateProjectCard(data)
+{
+    let temakorok = ""
+
+    data.topics.forEach(temakor => {
+        temakorok += `<li>${temakor}</li>`
+    });
+
+    console.log(data.repo_link);
+    console.log(data.site_link);
+
+    return `<div class="project-container">
+                <div class="project-layout">
+                    <div class="project-img">
+                        <img id="img-${data.id}" src="${data.photo_url}">
+                    </div>
+                    <div class="project-description">
+                        <h3>${data.title}</h3>
+                        <hr>
+                        <p>Témakörök</p>
+                        <ul>
+                            ${temakorok}
+                        </ul>
+
+                        <div class="project-links">
+                            <a href="${data.repo_link}" target="_blank" style="text-align: right;">Github link</a>
+                            <a href="${data.site_link}" target="_blank" style="text-align: right;">Weboldal</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function Render(json)
+{
+    const container = document.querySelector(".workContent")
+
+    json.forEach(data => {
+        container.innerHTML += CreateProjectCard(data)
+    });
+}
+
+getData(url, Render)
